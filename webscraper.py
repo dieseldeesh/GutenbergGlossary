11446921bfcc19a1a30449e7d6b3869cd6a96021
matthenricks <matthenricks@gmail.com>
2014-07-22 08:05:58 -0700

    Began adjusting for special characters in search

--- webscraper.py
+++ webscraper.py
@@ -4,11 +4,19 @@ import re
 from BeautifulSoup import BeautifulSoup
 from collections import namedtuple
 
+query = 'Tale of two cities'
+
+"all characters must be converted to text. \s --> +, rest become hex"
+"Spaces can even become text"
+def convertToSearchable(input):
+    hexVersion = input.encode("hex")
+    for i in range(len(hexVersion))/2:
+         
+
 BookMeta = namedtuple("BookMeta", "title author")
 
 splitter = re.compile(r'\s+')
 
-query = 'Tale of two cities'
 splitQuery = filter(None, splitter.split(query))
 str = '+'
 ending = str.join(splitQuery)
@@ -18,7 +26,7 @@ soup = BeautifulSoup(urllib2.urlopen(urlbase).read())
 
 bookListing = []
 
-for bookHolder in soup.findAll('li', {'class':'booklink'}):
+for bookHolder in soup.findAll('li', {'class':'boolink'}):
     bookInfo = bookHolder.find('span', {'class':'cell content'})
     title = bookInfo.find('span', {'class':'title'}).getText()
     if(bookInfo.find('span', {'class':'subtitle'}) != None):
