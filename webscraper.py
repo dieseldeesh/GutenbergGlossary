d55b16fe5ae4b292c7905f92abe147aa8fb4c1a4
Adhish Ramkumar <adhishramkumar@Adhishs-MacBook-Pro.local>
2014-07-22 00:06:57 -0700

    search results page added

--- webscraper.py
+++ webscraper.py
@@ -1,32 +1,20 @@
 import urllib2
-import re
-
 from BeautifulSoup import BeautifulSoup
 from collections import namedtuple
 
-BookMeta = namedtuple("BookMeta", "title author")
+Metadata = namedtuple("Metadata", "title author path")
 
-splitter = re.compile(r'\s+')
+data= urllib2.urlopen('file:///Users/adhishramkumar/Documents/GutenbergGlossary/test.html').read()
 
-query = 'Tale of two cities'
-splitQuery = filter(None, splitter.split(query))
-str = '+'
-ending = str.join(splitQuery)
-urlbase = 'http://www.gutenberg.org/ebooks/search/?query=' + ending
-
-soup = BeautifulSoup(urllib2.urlopen(urlbase).read())
-
-bookListing = []
-
-for bookHolder in soup.findAll('li', {'class':'booklink'}):
-    bookInfo = bookHolder.find('span', {'class':'cell content'})
-    title = bookInfo.find('span', {'class':'title'}).getText()
-    if(bookInfo.find('span', {'class':'subtitle'}) != None):
-        subtitle = bookInfo.find('span', {'class':'subtitle'}).getText()
-    bookListing.append(BookMeta(title, subtitle))
-
-
-"""
-  Get the HTML link
-  Put into the form of an onward list - print out HTML
-"""
+def getResults(query):
+	url = 'http://www.gutenberg.org/ebooks/search/?query=' + query.replace(' ','+')
+	html = data #urllib2.urlopen(url).read()
+	soup = BeautifulSoup(html)
+	bookListing = []
+	for bookHolder in soup.findAll('li', {'class':'booklink'}):
+		bookInfo = bookHolder.find('span', {'class':'cell content'})
+		title = bookInfo.find('span', {'class':'title'}).getText()
+		if(bookInfo.find('span', {'class':'subtitle'}) != None):
+			author = bookInfo.find('span', {'class':'subtitle'}).getText()
+		bookListing.append(Metadata(title=title, author=author, path="http://www.google.com"))
+	return bookListing
