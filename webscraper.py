9aab83c0bde16c16b04335888b6090161f6aa7fb
Adhish Ramkumar <adhishramkumar@Adhishs-MacBook-Pro.local>
2014-07-23 00:10:24 -0700

    changed url to point to html file and set up test.html path

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 from collections import namedtuple
+import os
 
 "all characters must be converted to hex so that the site can read them upon search"
 def convertToSearchable(input):
@@ -8,7 +9,8 @@ def convertToSearchable(input):
     return '%' + "%".join([hexVersion[start:start+2] for start in range(0, len(hexVersion), 2)])
 
 Metadata = namedtuple("Metadata", "title author path")
-data= urllib2.urlopen('file:///C:/Users/MOREPOWER/Desktop/Gutenberg Glossary/GutenbergGlossary/test.html').read()
+path = "file://" + os.path.dirname(os.path.realpath(__file__)) + '/test.html'
+data= urllib2.urlopen(path).read()
 
 gutenRootURL = "http://www.gutenberg.org"
 
@@ -25,5 +27,7 @@ def getResults(query):
 		"FIND THAT HTML"
 		if(bookInfo.find('span', {'class':'subtitle'}) != None):
 			author = bookInfo.find('span', {'class':'subtitle'}).getText()
-		bookListing.append(Metadata(title=title, author=author, path=bookUrl))
+		num = bookUrl.split("/")[-1]
+		htmlPath = "http://www.gutenberg.org/files/"+num+"/"+num+"-h/"+num+"-h.htm"
+		bookListing.append(Metadata(title=title, author=author, path=htmlPath))
 	return bookListing
